d32ccb5b696029baf6d4033acc459ef3abce18e6
kavsfi <chandpatelbca@gmail.com>
2023-08-29 18:38:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+/*let user1 = prompt("enter the name ");
+let fName = "Sri";
+let lName = "Ram";
+
+console.log("display the Name:",user1,fName,lName);
+document.write("Bolo " +user1 +fName +lName); 
+
+
+
+
+let x= 2;
+let y= 5;
+let z= 6;
+let a=x+y+z;
+
+console.log("display the cal:",a);
+document.write("the total is " +a); 
+
+
+let x= 2;
+let y= 5;
+let z= 6;
+let a=x-y-z;
+
+console.log("display the cal:",a);
+document.write("the total is " -a);
+
+
+let x= 2;
+let y= 5;
+let z= 6;
+let a=x*y*z;
+
+console.log("display the cal:",a);
+document.write("the total is " *a);
+
+
+
+
+
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+
+
+
+
+let a = prompt("enter the number" );
+if(a%2==0)
+{
+    console.log("the number is even");
+    document.write("the even number is:");
+}else{
+console.log("the number is odd");
+document.write("the odd number is ");
+}
+
+
+
+let fNum =5;
+let sNum =9;
+for(i=0;i<=fNum;i++)
+{
+sNum=sNum+i;
+console.log(sNum);
+//document.write(sNum)
+}
+
+
+
+let a = prompt("Enter the Number ");
+if(a>0){
+    console.log("positive ");
+}else{
+    console.log("negative");
+    window.alert("")
+}
+*/
+
+
+
+a= parseInt(prompt("enter the value"));
+
+b= parseInt(prompt("enter the value"));
+ c=a+b;
+ console.log("the datatype in c is " +typeof(c));
+ document.write("the value of c is" +c);
+
+window.alert("you value is "+c)
